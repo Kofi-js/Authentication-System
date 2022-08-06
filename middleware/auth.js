ebefbf41a972fbffdd1cb7dec9813fdd7b71466e
saint <saintmoses100@gmail.com>
2022-08-06 17:12:53 -0300

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,70 +1,63 @@
-// This middleware checks if there's a token and header
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
-const { SECRET } = process.env;
 
-exports.authenticateUser = (req, res, next) => {
-  // get token from header
-  const authorization_token = req.header("Authorization"); // or Authorization //x-auth-token
 
-  // check if tokeen doesn't exist
-  if (!authorization_token)
-    return res.status(401).json({
-      statusCode: 401,
-      message: "No token, authorization denied!",
-    });
+const verifyToken = (req,res,next)=>{
+    const authHeader = req.headers.token;
+    if (authHeader) {
+        const token = authHeader.split(" ")[1]
+        jwt.verify(token,process.env.JWT_SEC,(err,user)=>{
+            if(err) res.status(403).json("Token is not valid");
+            req.user = user;
+            next()
+        })
 
-  // check if token conntains Bearer
-  let splittedHeader = req.header("Authorization").split(" ");
-  if (splittedHeader[0] !== "Bearer")
-    return res.status(401).json({
-      statusCode: 401,
-      message: "authorization format is Bearer <token>",
-    });
-
-  // else... token exists
-  try {
-    let token = splittedHeader[1];
-    const decoded = jwt.verify(token, SECRET);
-
-    // assign user to request object
-    req.user = decoded.user;
-
-    next();
-  } catch (err) {
-    res.status(401).json({
-      statusCode: 401,
-      message: "Token is invalid!",
-    });
-  }
+    }else{
+        return res.status(401).json("You are not authenticated")
+    }
 };
 
-exports.checkIfAdmin = (req, res, next) => {
-  if (req.user.userRole !== "admin" && req.user.isAdmin !== true)
-    return res.status(401).json({
-      statusCode: 401,
-      message: "Route restricted to admin",
+
+const isAuth = (req,res,next)=>{
+    verifyToken(req,res,()=>{
+        if (req.user.id===req.params.id || req.user.isAdmin) {
+            next
+        }else{
+            res.status(403).json("You are not authorized")
+        }
     });
+}
 
-  return next();
-};
 
-exports.checkIfManager = (req, res, next) => {
-  if (req.user.userRole !== "manager" && req.user.isManager !== true)
-    return res.status(401).json({
-      statusCode: 401,
-      message: "Route restricted to managers",
+
+const isAdmin = (req,res,next)=>{
+    verifyToken(req,res,()=>{
+        if ( req.user.isAdmin) {
+            next();
+        }else{
+            res.status(403).json("You are not authorized")
+        }
     });
+}
 
-  return next();
-};
-
-exports.checkIfStaff = (req, res, next) => {
-  if (req.user.userRole !== "staff" && req.user.isStaff !== true)
-    return res.status(401).json({
-      statusCode: 401,
-      message: "Route restricted to staff users",
+const isManager = (req,res,next)=>{
+    verifyToken(req,res,()=>{
+        if(req.user.isManager){
+            next()
+        }else{
+            res.status(403).json("You are not authorized")
+        }
     });
+}
 
-  return next();
-};
+const isStaff = (req,res,next)=>{
+    verifyToken(req,res,()=>{
+        if(req.user.isStaff){
+            next()
+        }else{
+            res.status(403).json("Staffs only")
+        }
+    });
+}
+
+module.exports = {verifyToken,isAuth,isAdmin,isManager,isStaff};
+
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const { json } = require("express");
 const connectDB = (require = "./db");
 const routes = require("./routes/user");
+const debug = require('debug');
+
+debug(express);
+
 const port = process.env.PORT;
 require("dotenv").config();
 
